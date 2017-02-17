bb67c32c5fdd0b656859c8c4670d9847efd196f3
emfuthal <ewaldboss@hotmail.com>
2017-02-17 17:20:24 +0100

    new approach of link polling. new problem of empty xmlhttp response. maybe because of disallowed cross domain request

--- yt.js
+++ yt.js
@@ -1,37 +1,41 @@
-var patternBlock = "*://*.youtube.com/*/block";  // append '/block' to urls of songs you want to block and press enter
-var patternReset = "*://*.youtube.com/*/reset"; // appent '/reset' to any youtube url in order to reset the list of blocked urls
-var startUrl = "https://www.youtube.com/";
-var URLS = [];
+var PATTERN_BLOCK = "*://*.youtube.com/*/block";  // append '/block' to urls of songs you want to block and press enter
+var PATTERN_RESET = "*://*.youtube.com/*/reset"; // appent '/reset' to any youtube url in order to reset the list of blocked urls
+var YOUTUBE_STARTPAGE = "https://www.youtube.com/";
+var ANY_YOUTUBE_ADDRESS = "*://*.youtube.com/*";
+var ANY_YOUTUBE_SONG = "*://*.youtube.com/watch?*";
+
+var BlockedUrls = [];
+var PageUrls = [];
 
 
-function createCookie(value) {
+function createCookie( value ) {
     var expires;
     var date = new Date();
     date.setTime(date.getTime() + (100 * 24 * 60 * 60 * 1000));
     expires = "; expires=" + date.toGMTString();
-    document.cookie = "URLS=" + value + expires + "; path=/";
+    document.cookie = "BlockedUrls=" + value + expires + "; path=/";
 }
 
-function getCookie(c_name) {
+function getCookie( cookieName ) {
     if (document.cookie.length > 0) {
-        c_start = document.cookie.indexOf(c_name + "=");
-        if (c_start != -1) {
-            c_start = c_start + c_name.length + 1;
-            c_end = document.cookie.indexOf(";", c_start);
-            if (c_end == -1) {
-                c_end = document.cookie.length;
+        cookieStart = document.cookie.indexOf(cookieName + "=");
+        if (cookieStart != -1) {
+            cookieStart = cookieStart + cookieName.length + 1;
+            cookieEnd = document.cookie.indexOf(";", cookieStart);
+            if (cookieEnd == -1) {
+                cookieEnd = document.cookie.length;
             }
-            return unescape(document.cookie.substring(c_start, c_end));
+            return unescape(document.cookie.substring(cookieStart, cookieEnd));
         }
     }
     return "";
 }
 
-function delete_cookie( name ) {
+function deleteCookie( name ) {
     document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
-function getUrls(){
+function getUrlsFromCookie(){
     if (document.cookie) {
 	var json_str = getCookie('Urls');
 	var arr = JSON.parse(json_str);
@@ -41,48 +45,50 @@ function getUrls(){
     }
 }
 
-function storeUrls( urls ) {
-    var json_str = JSON.stringify(urls);
+function storeUrlsInCookie( ArrayOfUrls ) {
+    var json_str = JSON.stringify(ArrayOfUrls);
     createCookie(json_str);
 }
 
-function getAllLinks( url ) {
+function prepareNextSong( requestDetails ) {
     // request the youtube page of the current song
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url, false);
-    xhttp.send();
+    xhttp.open("GET", requestDetails.url, true);
     
-    // create a DOM object from the HTML response
-    var doc = document.implementation.createHTMLDocument("youtubePage");
-    doc.documentElement.innerHTML = xhttp.responseText;
-
-    // get the links of the current youtube page
-    links = doc.links;
-    var arr = [];
-    for(var i = 0; i < links.length; i++) {
-	if (~(links[i].indexOf(startUrl.concat("*")))){
-	    arr.push(links[i].href);
-	}
-    }
-    return arr;
+    xhttp.onreadystatechange = function () {
+        if(xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
+            // create a DOM object from the HTML response
+	    var doc = document.implementation.createHTMLDocument("youtubePage");
+	    doc.documentElement.innerHTML = xhttp.responseText;
+	    
+	    // get the links of the current youtube page
+	    links = doc.links;
+	    pageUrls = [];
+	    for(var i = 0; i < links.length; i++) {
+		if (~(links[i].indexOf(ANY_YOUTUBE_SONG))){ // is element i of links a link to a youtube song?
+		    pageUrls.push(links[i].href);
+		}
+	    }
+        }
+    };
+    xhttp.send();
 }
 
-function getRandomNextSong( url ){
-    var links = getAllLinks( url );
-    var s = links.length;
+function playRandomNextSong( arrayOfSongLinks ){
+    var s = arrayOfSongLinks.length;
     var index = Math.floor(Math.random() * s);
-    return links[index];
+    return arrayOfSongLinks[index];
 }
 
 function reqListener () {
   console.log(this.responseText);
 }
 
-function update(requestDetails) {
-    URLS = getUrls();
+function update( requestDetails ) {
+    BlockedUrls = getUrlsFromCookie();
 }
 
-function redirect(requestDetails) {
+function redirect( requestDetails ) {
     var url = requestDetails.url;
     console.log("Redirecting: " + url);
     var nextUrl = getRandomNextSong( url );
@@ -91,41 +97,50 @@ function redirect(requestDetails) {
     };
 }
 
-
-function block(requestDetails){
+function block( requestDetails ){
     var curl = requestDetails.url;
     var url = curl.substring(0, curl.length - 6);
-    URLS.push(url);
-    storeUrls(URLS);
-    var nextUrl = getRandomNextSong( url );
+    BlockedUrls.push(url);
+    storeUrlsInCookie(BlockedUrls);
+    var nextUrl = playRandomNextSong(pageUrls);
     console.log("Redirecting: " + url);
     return {
 	redirectUrl: nextUrl
     };
 }
 
-function resetUrlList(requestDetails){
+function resetUrlList( requestDetails ){
     delete_cookie('Urls');
 }
 
+// get the list of blocked urls from storage when starting youtube (on startpage).
 browser.webRequest.onBeforeRequest.addListener(
     update,
-    {urls:[startUrl]}
+    {urls:[YOUTUBE_STARTPAGE]}
 );
 
+// redirect the requested url to anoter url if this url is blocked
 browser.webRequest.onBeforeRequest.addListener(
     redirect,
-    {urls:URLS},
+    {urls:BlockedUrls},
     ["blocking"]
 );
 
+// prepare a song to play in case the next autoplayed song is blocked
+browser.webRequest.onCompleted.addListener(
+    prepareNextSong,
+    {urls:[ANY_YOUTUBE_SONG]}
+);
+
+// add a url to the list of blocked urls
 browser.webRequest.onBeforeRequest.addListener(
     block,
-    {urls:[patternBlock]},
+    {urls:[PATTERN_BLOCK]},
     ["blocking"]
 );
 
+// delete the list of all blocked urls.
 browser.webRequest.onBeforeRequest.addListener(
     resetUrlList,
-    {urls:[patternReset]}
+    {urls:[PATTERN_RESET]}
 );

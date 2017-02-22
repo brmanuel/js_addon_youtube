57340639c5f0bbc317d33382b9d410f62a18e77b
emfuthal <ewaldboss@hotmail.com>
2017-02-22 23:22:33 +0100

    tried re-accessing youtube to get links

--- yt.js
+++ yt.js
@@ -7,6 +7,9 @@ var ANY_YOUTUBE_SONG = "*://*.youtube.com/watch?*";
 var BlockedUrls = [];
 var PageUrls = [];
 
+var request = require('request');
+var cheerio = require('cheerio');
+
 
 function createCookie( value ) {
     var expires;
@@ -51,8 +54,22 @@ function storeUrlsInCookie( ArrayOfUrls ) {
 }
 
 function prepareNextSong( requestDetails ) {
+
+    console.log("Visiting page " + url);
+    request(url, function(error, response, body) {
+     // Check status code (200 is HTTP OK)
+     console.log("Status code: " + response.statusCode);
+     if(response.statusCode !== 200) {
+       callback();
+       return;
+     }
+     // Parse the document body
+	var $ = cheerio.load(body);
+	var links = $.links;
+  });
+    
     // request the youtube page of the current song
-    var xhttp = new XMLHttpRequest();
+    /*var xhttp = new XMLHttpRequest();
     xhttp.open("GET", requestDetails.url, true);
     
     xhttp.onreadystatechange = function () {
@@ -71,7 +88,7 @@ function prepareNextSong( requestDetails ) {
 	    }
         }
     };
-    xhttp.send();
+    xhttp.send();*/
 }
 
 function playRandomNextSong( arrayOfSongLinks ){
